1df9d9a90408ea8c36564d7b1e127cc6655d0484
LinYenYen <65504540+LinYenYen@users.noreply.github.com>
2020-06-13 12:48:34 +0800

    Add files via upload

---
+++ HSV_convert_RGB.py
@@ -0,0 +1,86 @@
+def hsv_to_rgb(color: str):
+    color_type = color[:3]
+    color_list = color[4:-1].split(',')
+    # hsv convert to rgb
+    if color_type == 'hsv':
+        h = float(color_list[0].strip())
+        s = float(color_list[1].strip().strip('%')) / 100
+        v = float(color_list[2].strip().strip('%')) / 100
+        hi = int((h / 60) % 6)
+        f = (h / 60) - hi
+        p = v * (1 - s)
+        q = v * (1 - f * s)
+        t = v * (1 - (1 - f) * s)
+        # r, g, b
+        if hi == 0:
+            r = v
+            g = t
+            b = p
+        elif hi == 1:
+            r = q
+            g = v
+            b = p
+        elif hi == 2:
+            r = p
+            g = v
+            b = t
+        elif hi == 3:
+            r = p
+            g = q
+            b = v
+        elif hi == 4:
+            r = t
+            g = p
+            b = v
+        elif hi == 5:
+            r = v
+            g = p
+            b = q
+        else:
+            print('Error Input Form!')
+        r = round(r*255)
+        g = round(g*255)
+        b = round(b*255)
+        return 'rgb({}, {}, {})'.format(r, g, b)
+    # rgb convert to hsv
+    elif color_type == 'rgb':
+        r = float(color_list[0].strip()) / 255
+        g = float(color_list[1].strip()) / 255
+        b = float(color_list[2].strip()) / 255
+        Max = max(r, g, b)
+        Min = min(r, g, b)
+        # h
+        if Max == Min:
+            h = 0
+        elif (Max == r) & (g >= b):
+            h = 60 * ((g - b) / (Max - Min)) + 0
+        elif (Max == r) & (g < b):
+            h = 60 * ((g - b) / (Max - Min)) + 360
+        elif Max == g:
+            h = 60 * ((b - r) / (Max - Min)) + 120
+        elif Max == b:
+            h = 60 * ((r - g) / (Max - Min)) + 240
+        else:
+            print('Error Input Form!')
+        # s
+        if Max == 0:
+            s = 0
+        else:
+            s = 1 - (Min / Max)
+        # v
+        v = Max
+        # h, s, v
+        h = round(h)
+        s = round(s * 100)
+        v = round(v * 100)
+        return 'hsv({}, {}%, {}%)'.format(h, s, v)
+    else:
+        print('Error Input Form!')
+
+if __name__ == '__main__':
+    hsv = 'hsv(0, 50%, 10%)'
+    rgb = 'rgb(0, 0, 104)'
+    a = hsv_to_rgb(hsv)
+    b = hsv_to_rgb(rgb)
+    print(a)
+    print(b)
